65fa24549b22f088d249af179ef8b68500defe55
Eatgrapes <Eatgrapes232@gmail.com>
2025-07-12 18:47:42 +0800

    update language

--- src/main/java/com/soarclient/management/mod/impl/settings/ModMenuSettings.java
+++ src/main/java/com/soarclient/management/mod/impl/settings/ModMenuSettings.java
@@ -24,81 +24,108 @@ import net.minecraft.client.util.InputUtil;
   
 public class ModMenuSettings extends Mod {  
   
-	private static ModMenuSettings instance;  
+    private static ModMenuSettings instance;  
+    private String previousLanguageOption = "English";  
   
-	private KeybindSetting keybindSetting = new KeybindSetting("setting.keybind", "setting.keybind.description",  
-			Icon.KEYBOARD, this, InputUtil.fromKeyCode(GLFW.GLFW_KEY_RIGHT_SHIFT, 0));  
-	private BooleanSetting darkModeSetting = new BooleanSetting("setting.darkmode", "setting.darkmode.description",  
-			Icon.DARK_MODE, this, false);  
-	private HctColorSetting hctColorSetting = new HctColorSetting("setting.color", "setting.color.description",  
-			Icon.PALETTE, this, Hct.from(220, 26, 6));  
-	private BooleanSetting blurSetting = new BooleanSetting("setting.blur", "setting.blur.description", Icon.LENS_BLUR,  
-			this, true);  
-	private NumberSetting blurIntensitySetting = new NumberSetting("setting.blurintensity",  
-			"setting.blurintensity.description", Icon.BLUR_LINEAR, this, 5, 1, 20, 1);  
-	private ComboSetting languageSetting = new ComboSetting("setting.language", "setting.language.description",  
-			Icon.LANGUAGE, this, Arrays.asList("English", "中文"), "English");  
+    private KeybindSetting keybindSetting = new KeybindSetting("setting.keybind", "setting.keybind.description",  
+            Icon.KEYBOARD, this, InputUtil.fromKeyCode(GLFW.GLFW_KEY_RIGHT_SHIFT, 0));  
+    private BooleanSetting darkModeSetting = new BooleanSetting("setting.darkmode", "setting.darkmode.description",  
+            Icon.DARK_MODE, this, false);  
+    private HctColorSetting hctColorSetting = new HctColorSetting("setting.color", "setting.color.description",  
+            Icon.PALETTE, this, Hct.from(220, 26, 6));  
+    private BooleanSetting blurSetting = new BooleanSetting("setting.blur", "setting.blur.description", Icon.LENS_BLUR,  
+            this, true);  
+    private NumberSetting blurIntensitySetting = new NumberSetting("setting.blurintensity",  
+            "setting.blurintensity.description", Icon.BLUR_LINEAR, this, 5, 1, 20, 1);  
+    private ComboSetting languageSetting = new ComboSetting("setting.language", "setting.language.description",  
+            Icon.LANGUAGE, this, Arrays.asList("English", "中文"), "English");  
   
-	private Screen modMenu;  
+    private Screen modMenu;  
   
-	public ModMenuSettings() {  
-		super("mod.modmenu.name", "mod.modmenu.description", Icon.MENU, ModCategory.MISC);  
+    public ModMenuSettings() {  
+        super("mod.modmenu.name", "mod.modmenu.description", Icon.MENU, ModCategory.MISC);  
   
-		instance = this;  
-		this.setHidden(true);  
-		this.setEnabled(true);  
-	}  
+        instance = this;  
+        this.setHidden(true);  
+        this.setEnabled(true);  
+          
+        // init language 
+        initializeLanguageSetting();  
+    }  
   
-	public final EventBus.EventListener<ClientTickEvent> onClientTick = event -> {  
+    private void initializeLanguageSetting() {  
+        Language currentLang = I18n.getCurrentLanguage();  
+        if (currentLang != null) {  
+            String langOption = currentLang == Language.CHINESE ? "中文" : "English";  
+            languageSetting.setOption(langOption);  
+            previousLanguageOption = langOption;  
+        }  
+    }  
   
-		if (keybindSetting.isPressed()) {  
+    public final EventBus.EventListener<ClientTickEvent> onClientTick = event -> {  
   
-			if (modMenu == null) {  
-				modMenu = new GuiModMenu().build();  
-			}  
+        if (keybindSetting.isPressed()) {  
   
-			client.setScreen(modMenu);  
-		}  
+            if (modMenu == null) {  
+                modMenu = new GuiModMenu().build();  
+            }  
   
-		// Handle language switching  
-		String currentLanguageOption = languageSetting.getOption();  
-		Language targetLanguage = currentLanguageOption.equals("中文") ? Language.CHINESE : Language.ENGLISH;  
-		  
-		if (I18n.getCurrentLanguage() != targetLanguage) {  
-			I18n.setLanguage(targetLanguage);  
-		}  
-	};  
+            client.setScreen(modMenu);  
+        }  
   
-	@Override  
-	public void onDisable() {  
-		this.setEnabled(true);  
-	}  
+        // Language Handler 
+        handleLanguageChange();  
+    };  
   
-	public static ModMenuSettings getInstance() {  
-		return instance;  
-	}  
+    private void handleLanguageChange() {  
+        String currentLanguageOption = languageSetting.getOption();  
+           
+        if (!currentLanguageOption.equals(previousLanguageOption)) {  
+            Language targetLanguage = currentLanguageOption.equals("中文") ? Language.CHINESE : Language.ENGLISH;  
+              
+            if (I18n.getCurrentLanguage() != targetLanguage) {  
+                I18n.setLanguage(targetLanguage);  
+                  
+                // rebuild modui  
+                if (modMenu != null) {  
+                    modMenu = null; 
+                }  
+            }  
+              
+            previousLanguageOption = currentLanguageOption;  
+        }  
+    }  
   
-	public BooleanSetting getDarkModeSetting() {  
-		return darkModeSetting;  
-	}  
+    @Override  
+    public void onDisable() {  
+        this.setEnabled(true);  
+    }  
   
-	public HctColorSetting getHctColorSetting() {  
-		return hctColorSetting;  
-	}  
+    public static ModMenuSettings getInstance() {  
+        return instance;  
+    }  
   
-	public BooleanSetting getBlurSetting() {  
-		return blurSetting;  
-	}  
+    public BooleanSetting getDarkModeSetting() {  
+        return darkModeSetting;  
+    }  
   
-	public NumberSetting getBlurIntensitySetting() {  
-		return blurIntensitySetting;  
-	}  
+    public HctColorSetting getHctColorSetting() {  
+        return hctColorSetting;  
+    }  
   
-	public ComboSetting getLanguageSetting() {  
-		return languageSetting;  
-	}  
+    public BooleanSetting getBlurSetting() {  
+        return blurSetting;  
+    }  
   
-	public Screen getModMenu() {  
-		return modMenu;  
-	}  
+    public NumberSetting getBlurIntensitySetting() {  
+        return blurIntensitySetting;  
+    }  
+  
+    public ComboSetting getLanguageSetting() {  
+        return languageSetting;  
+    }  
+  
+    public Screen getModMenu() {  
+        return modMenu;  
+    }  
 }
